76aaf7a20ffe9e968c6901e79d2acda56f1d2af9
SaiKoteshVasa <128318033+SaiKoteshVasa@users.noreply.github.com>
2023-06-05 17:39:14 +0530

    Create set11.py

---
+++ set11.py
@@ -0,0 +1,51 @@
+#1
+import pandas as pd
+#pd.set_option('display.max_rows', None)
+#pd.set_option('display.max_columns', None)
+student_data = pd.DataFrame({
+    'school_code': ['s001','s002','s003','s001','s002','s004'],
+    'class': ['V', 'V', 'VI', 'VI', 'V', 'VI'],
+    'name': ['Alberto Franco','Gino Mcneill','Ryan Parkes', 'Eesha Hinton', 'Gino Mcneill', 'David Parkes'],
+    'date_Of_Birth ': ['15/05/2002','17/05/2002','16/02/1999','25/09/1998','11/05/2002','15/09/1997'],
+    'age': [12, 12, 13, 13, 14, 12],
+    'height': [173, 192, 186, 167, 151, 159],
+    'weight': [35, 32, 33, 30, 31, 32],
+    'address': ['street1', 'street2', 'street3', 'street1', 'street2', 'street4']},
+    index=['S1', 'S2', 'S3', 'S4', 'S5', 'S6'])
+
+print("Original DataFrame:")
+print(student_data)
+print('\nSplit the said data on school_code wise:')
+result = student_data.groupby(['school_code'])
+for name,group in result:
+    print("\nGroup:")
+    print(name)
+    print(group)
+print("\nType of the object:")
+print(type(result))
+
+#2
+import numpy as np
+a=np.array([[8,3],[6,9]])
+b=np.array([[2,5],[7,8]])
+print("Add:")
+print(np.add(a,b))
+print("Subtract:")
+print(np.subtract(a,b))
+print("Multiply:")
+print(np.multiply(a,b))
+print("Divide:")
+print(np.divide(a,b))
+c=np.array([[4,16],[9,25]])
+print("Sqrt:")
+print(np.sqrt(c))
+d=np.array([1,30,45,60,90])
+print("sin:",np.sin(d*np.pi/180))
+print("cos:",np.cos(d*np.pi/180))
+e=np.array([2,4,3,6])
+print("log:",np.log(e))
+p=np.array([3,8j])
+q=np.array([7,2j])
+print("Dot:",np.dot(p,q))
+r=np.array([1,-1,-6])
+print("Roots:",np.roots(r))
